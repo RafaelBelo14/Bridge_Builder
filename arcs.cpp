e3d0cf6cd80154ac00498f53d82e5562bd215fde
Rafael Baptista <rafael14belo@gmail.com>
2021-04-25 17:50:10 +0100

    140 points

--- arcs.cpp
+++ arcs.cpp
@@ -1,3 +1,17 @@
+/*
+
+Tipos de sentidos:
+
+N - Null
+S - Subir
+D - Descer
+A - Ambos
+
+1º e última coluna são fixas pelo que são apenas calculadas as soluções em altura 0 nesses casos.
+Ciclos for percorridos apenas pelas células que têm um bloco em comum com a em qustão.
+
+*/
+
 #include <iostream>
 #include <vector>
 #include <string> 
@@ -5,13 +19,15 @@ using namespace std;
 
 struct celula {
       char sentido;
-      int valor_subida;
-      int valor_descida;
+      long valor_subida;
+      long valor_descida;
 };
 
 vector<int> test; 
 
 int doCase(int n, int h, int H);
+int mod_abs(int a, int mod);
+int mod_add(int a, int b, int mod);
 
 int main()
 {
@@ -27,11 +43,18 @@ int main()
             cin >> h;
             cin >> H;
 
-            if (n > 500 || h > 500 || H > 60000) {
+            // VERIFICA CONDIÇÕES INICIAIS
+            if (n > 500 || n <= 0 || h > 500 || h <= 0 || H > 60000 || H <= 0) {
                   return 0;
             }
 
-            test.push_back(doCase(n, h, H));
+            // VERIFICA SOLUÇÕES IMPOSSÍVEIS
+            if (h == H || h > H || n < 2) {
+                  test.push_back(0);
+            }
+            else {
+                test.push_back(doCase(n, h, H));  
+            }
       }
 
       for (auto i = test.begin(); i != test.end(); ++i) 
@@ -46,70 +69,102 @@ int doCase(int n, int h, int H) {
       int lines = n;
       int columns = H;
       int blockHeight = h;
-      struct celula matriz[lines][columns];
+      int k;
+
+      // ALLOCA MEMÓRIA PARA O ARRAY DE ARRAY'S
+      struct celula ** matriz = (struct celula ** )malloc(lines * sizeof(struct celula *));
       int solutions = 0;
 
       for (int i = 0; i < lines; i++) {
+
+            //ALOCA MEMÓRIA PARA UM NOVO ARRAY QUE SERÁ UMA LINHA
+            matriz[i] = (struct celula *)malloc(columns * sizeof(struct celula));
+
             for (int j = 0; j < columns; j++) {
-                  if (i == 0 && j == 0) {
-                        matriz[i][j].sentido = 'S';
-                        matriz[i][j].valor_subida = 1;
-                        matriz[i][j].valor_descida = 0; 
+
+                  if (i == 0) {
+                        if (j == 0) {
+                              matriz[i][j].sentido = 'S';
+                              matriz[i][j].valor_subida = 1;
+                              matriz[i][j].valor_descida = 0; 
+                        }
+                        else {
+                              matriz[i][j].sentido = 'N';
+                              matriz[i][j].valor_subida = 0;
+                              matriz[i][j].valor_descida = 0;
+                        }
+                        break;
                   }
                   else {
+
+                        // GUARDA NA CÉLULAS OS VALORES 'DEFAULT'
                         matriz[i][j].sentido = 'N';
                         matriz[i][j].valor_subida = 0;
-                        matriz[i][j].valor_descida = 0; 
-                  }
-            }
-      }
+                        matriz[i][j].valor_descida = 0;
 
-      for (int i = 1; i < lines; i++) {
-            for (int j = 0; j < columns; j++) {
-
-                  if (i < lines - 1) {
                         if (j + (blockHeight - 1) < columns) {
-                              for (int k = 0; k < columns; k++) {
-                                    if (matriz[i - 1][k].sentido == 'S' && k < j && k + blockHeight > j) {
-                                          matriz[i][j].sentido = 'S';
-                                          matriz[i][j].valor_subida += 1;
+
+                              // VALOR PARA PERCORRER NO CICLO FOR (IMPEDE DE SE PERCORRA A LINHA TODA)
+                              if (j - (blockHeight - 1) < 0) {
+                                    k = 0;
+                              }
+                              else {
+                                    k = j - (blockHeight - 1);
+                              }
+
+                              //COLUNAS INTERMÉDIAS
+                              if (i < lines - 1) {
+
+                                    for (; k < j + blockHeight; k++) {
+                                          
+                                          // SÓ SOBE
+                                          if ((matriz[i - 1][k].sentido == 'S' || matriz[i - 1][k].sentido == 'A') && k < j && k + blockHeight > j) {
+                                                matriz[i][j].sentido = 'S';
+                                                matriz[i][j].valor_subida = mod_add(matriz[i][j].valor_subida, matriz[i - 1][k].valor_subida, 1000000007);
+                                          }
+                                          // DESCE E SOBE
+                                          if ((matriz[i - 1][k].sentido == 'S' || matriz[i - 1][k].sentido == 'A') && j < k && j + blockHeight > k) {
+                                                if (matriz[i][j].sentido == 'S' || matriz[i][j].sentido == 'A') {
+                                                      matriz[i][j].sentido = 'A';
+                                                      matriz[i][j].valor_descida = mod_add(matriz[i][j].valor_descida, matriz[i - 1][k].valor_subida + matriz[i - 1][k].valor_descida, 1000000007);
+                                                }
+                                          } 
+                                          // SÓ DESCE
+                                          if ((matriz[i][j].sentido == 'N' || matriz[i][j].sentido == 'D') && j < k && j + blockHeight > k) {
+                                                if (matriz[i - 1][k].sentido == 'S' || matriz[i - 1][k].sentido == 'D' || matriz[i - 1][k].sentido == 'A') {
+                                                      matriz[i][j].sentido = 'D';
+                                                      matriz[i][j].valor_descida = mod_add(matriz[i][j].valor_descida, matriz[i - 1][k].valor_subida + matriz[i - 1][k].valor_descida, 1000000007);
+                                                }
+                                          }
                                     }
-                                    if (matriz[i - 1][k].sentido == 'S' && j < k && j + blockHeight > k) {
-                                          matriz[i][j].sentido = 'D';
-                                          matriz[i][j].valor_descida += 1;
+                              }
+
+                              // CASO SEJA A ÚLTIMA COLUNA SÓ CALCULA PARA A ALTURA 0
+                              else if (i == lines - 1 && j == 0){
+
+                                    for (; k < j + blockHeight; k++) {
+                                          if ((matriz[i - 1][k].sentido == 'S' || matriz[i - 1][k].sentido == 'A' || matriz[i - 1][k].sentido == 'D') && j < k && j + blockHeight > k) {
+                                                matriz[i][j].sentido = 'D';
+                                                matriz[i][j].valor_descida = mod_add(matriz[i][j].valor_descida, matriz[i - 1][k].valor_subida + matriz[i - 1][k].valor_descida, 1000000007);
+                                          }
                                     }
                               }
-                        }
-                  }
-                  else {
-                        for (int k = 0; k < columns; k++) {
-                              if (matriz[i - 1][k].sentido == 'S' && j < k && j + blockHeight > k) {
-                                    matriz[i][j].sentido = 'D';
-                                    matriz[i][j].valor_descida += 1;
+
+                              // ATUALIZA O TOTAL DE SOLUÇÕES
+                              if (j == 0) {
+                                    solutions = mod_add(solutions, matriz[i][j].valor_descida, 1000000007);
                               }
-                              if (matriz[i - 1][k].sentido == 'D' && j < k && j + blockHeight > k) {
-                                    matriz[i][j].sentido = 'D';
-                                    matriz[i][j].valor_descida += 1;
-                              }
-                        }
+                        } 
                   }
-     
             }
       }
+      return mod_abs(solutions, 1000000007);
+}
 
-      /*for (int i = 0; i < n; i++) {
-            for (int j = 0; j < H; j++) {
-                  cout << "(" << matriz[i][j].sentido << "," << matriz[i][j].valor_subida << "," << matriz[i][j].valor_descida << ")  ";
-            }
-            cout << endl;
-      }*/
+int mod_abs(int a, int mod) {
+  return ((a % mod) + mod) % mod;
+}
 
-      for (int i = 1; i < n; i++) {
-            solutions += matriz[i][0].valor_descida;
-      }
-
-      
-
-      return solutions;
-
+int mod_add(int a, int b, int mod) {
+  return (mod_abs(a, mod) + mod_abs(b, mod)) % mod;
 }
